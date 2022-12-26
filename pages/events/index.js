9f6e87239e5d9f1373ada31c8d79bee68f67ec8e
Fabiger <fabigerchanel@gmail.com>
2022-12-26 09:45:41 +0100

    1

---
+++ pages/events/index.js
@@ -0,0 +1,21 @@
+const Page = () => {
+    return <div>
+        <hi>Events Page</hi>
+        <div>
+        <a href="">
+          <img />
+          <h2> Events in London</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2> Events in San Fransisco</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2> Events in Barcelona</h2>
+        </a>
+        </div>
+    </div>
+}
+
+export default Page;
